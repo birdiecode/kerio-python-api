ee831d9e7b6ae007a3cd55d091b7ac50a5b84b64
birdiecode <birdiecode@protonmail.com>
2021-12-15 13:23:15 +0300

    добавлен метод запроса пользователей из Operator

--- kerio/operator.py
+++ kerio/operator.py
@@ -4,3 +4,6 @@ from .base import Kerio, PRODUCT_OPERATOR
 class KerioOperator(Kerio):
     def __init__(self, host, login, password, port=4021, authorization=True):
         super(KerioOperator, self).__init__(PRODUCT_OPERATOR, host, port, '/dologin.php', login, password, authorization)
+
+    def getUsers(self, start=0, limit=-1):
+        return self.request("Users.get", {"query": {"start": start, "limit": limit, "orderBy": [{"columnName": "EXTENSIONS", "direction": "Asc"}]}})['result']
