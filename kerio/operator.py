7f9fa32230fa48337418ef73253a57011516a4bf
birdiecode <birdiecode@protonmail.com>
2021-12-15 17:09:39 +0300

    [ KerioOperator ] добавлен метод getConferences (Конференции)

--- kerio/operator.py
+++ kerio/operator.py
@@ -6,7 +6,34 @@ class KerioOperator(Kerio):
         super(KerioOperator, self).__init__(PRODUCT_OPERATOR, host, port, '/dologin.php', login, password, authorization)
 
     def getUsers(self, start=0, limit=-1):
-        return self.request("Users.get", {"query": {"start": start, "limit": limit, "orderBy": [{"columnName": "EXTENSIONS", "direction": "Asc"}]}})['result']
+        return self.request("Users.get", {
+            "query": {
+                "start": start,
+                "limit": limit,
+                "orderBy": [
+                    {"columnName": "EXTENSIONS", "direction": "Asc"}
+                ]
+            }
+        })['result']
 
     def getExtensions(self, start=0, limit=-1):
-        return self.request("Extensions.get", {"query": {"start": start, "limit": limit, "orderBy": [{"columnName": "telNum", "direction": "Asc"}]}})['result']
+        return self.request("Extensions.get", {
+            "query": {
+                "start": start,
+                "limit": limit,
+                "orderBy": [
+                    {"columnName": "telNum", "direction": "Asc"}
+                ]
+            }
+        })['result']
+
+    def getConferences(self, start=0, limit=-1):
+        return self.request("Conferences.get", {
+            "query": {
+                "start": start,
+                "limit": limit,
+                "orderBy": [
+                    {"columnName": "EXTENSION_NUMBER", "direction": "Asc"}
+                ]
+            }
+        })['result']
