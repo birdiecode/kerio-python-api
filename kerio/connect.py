a8cb179a158a1561e6ffe7caee7c267ee8b33ad1
birdiecode <birdiecode@protonmail.com>
2021-12-16 11:50:55 +0300

    [ KerioConnect ] добавлен метод getDomains и getGroupListAccessPolicy

--- kerio/connect.py
+++ kerio/connect.py
@@ -4,3 +4,42 @@ from .base import Kerio, PRODUCT_CONNECT
 class KerioConnect(Kerio):
     def __init__(self, host, login, password, port=4040, authorization=True):
         super(KerioConnect, self).__init__(PRODUCT_CONNECT, host, port, '/login/dologin', login, password, authorization)
+
+    def getDomains(self):
+        return self.request("Domains.get", {
+            "query": {
+                "fields": [
+                    "id",
+                    "name",
+                    "service",
+                    "keepForRecovery",
+                    "isDistributed",
+                    "deletedItems",
+                    "junkEmail",
+                    "sentItems",
+                    "autoDelete",
+                    "isPrimary",
+                    "passwordExpirationEnabled",
+                    "passwordComplexityEnabled",
+                    "passwordMinimumLength",
+                    "passwordHistoryCount",
+                    "isLdapManagementAllowed"
+                ],
+                "orderBy": [
+                    {
+                        "columnName": "name",
+                        "direction": "Asc"
+                    }
+                ],
+                "start": 0,
+                "limit": -1
+            }
+        })['result']
+
+    def getGroupListAccessPolicy(self, start=0, limit=-1):
+        return self.request("AccessPolicy.getGroupList", {
+            "query": {
+                "start": start,
+                "limit": limit
+            }
+        })['result']
