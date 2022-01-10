42f180d01e798ede3aa0bf056879cb4dcc81a8ec
birdiecode <birdiecode@protonmail.com>
2022-01-10 05:59:30 +0300

    [ KerioConnect ][ fix ] испровлен метод createUsers, добавленн вспомогательные классы KerioConnectToolDomainsKerioConnectToolAccessPolicy

--- kerio/__init__.py
+++ kerio/__init__.py
@@ -1,2 +1,6 @@
 from .connect import KerioConnect
+from .connect import KerioConnectToolMakeUser
+from .connect import KerioConnectToolDomains
+from .connect import KerioConnectToolAccessPolicy
+
 from .operator import KerioOperator
--- kerio/connect.py
+++ kerio/connect.py
@@ -1,6 +1,121 @@
 from .base import Kerio, PRODUCT_CONNECT
 
 
+class KerioConnectToolMakeUser():
+    skeleton = {
+        "loginName": None,
+        "description": None,
+        "hasDefaultSpamRule": True,
+        "allowPasswordChange": True,
+        "publishInGal": True,
+        "authType": "UInternalAuth",
+        "isPasswordReversible": True,
+        "isEnabled": True,
+        "domainId": None,
+        "hasDomainRestriction": False,
+        "accessPolicy": None,
+        "password": None,
+        "role": {
+            "userRole": "UserRole",
+            "publicFolderRight": False,
+            "archiveFolderRight": False
+        },
+        "itemLimit": {
+            "isActive": False,
+            "limit": 0
+        },
+        "diskSizeLimit": {
+            "isActive": False,
+            "limit": {
+                "value": 0,
+                "units": "MegaBytes"    
+            }
+        },
+        "outMessageLimit": {
+            "isActive": False,
+            "limit": {
+                "value": 0,
+                "units": "MegaBytes"
+            }
+        },
+        "cleanOutItems": {
+             "isUsedDomain": True,
+             "deletedItems": {
+                "isEnabled": False,
+                "days": 30
+            },
+            "junkEmail": {
+                "isEnabled": False,
+                "days": 30
+            },
+            "sentItems": {
+                "isEnabled": False,
+                "days": 30
+            },
+            "autoDelete": {
+                "isEnabled": False,
+                "days": 1095
+            }
+        },
+        "fullName": None,
+        "emailForwarding": {
+            "mode": "UForwardNone",
+            "emailAddresses": []
+        },
+        "emailAddresses": [],
+        "userGroups": [],
+        "companyContactId": ""
+    }
+
+    def __init__(self, fullName=None, loginName=None, description=None, domainId=None, accessPolicy=None, password=None):
+        self.skeleton['fullName']=fullName
+        self.skeleton['loginName']=loginName
+        self.skeleton['description']=description
+        self.skeleton['domainId']=domainId
+        self.skeleton['accessPolicy']=accessPolicy
+        self.skeleton['password']=password
+
+
+class KerioConnectToolAccessPolicy:
+    def __init__(self, ww):
+        self.ww = ww
+
+    def __iter__(self):
+        self.indx = 0
+        return self
+
+    def __next__(self):
+        ee = self.ww[self.indx]
+        self.indx += 1
+        return ee
+
+    def getDefault(self):
+        for ee in self.ww:
+            if ee['isDefault']:
+                return ee
+        raise Exception("not found access policy")
+
+
+class KerioConnectToolDomains:
+    def __init__(self, ww):
+        self.ww = ww
+
+    def __iter__(self):
+        self.indx = 0
+        return self
+
+    def __next__(self):
+        ee = self.ww[self.indx]
+        self.indx+=1
+        return ee
+
+    def get(self, domain):
+        for ee in self.ww:
+            if ee["name"] == domain:
+                return ee['id']
+        raise Exception("not found domain")
+
+
 class KerioConnect(Kerio):
     def __init__(self, host, login, password, port=4040, authorization=True):
         super(KerioConnect, self).__init__(PRODUCT_CONNECT, host, port, '/login/dologin', login, password, authorization)
@@ -44,7 +159,7 @@ class KerioConnect(Kerio):
             }
         })['result']
 
-    def createUsers():
+    def createUsers(self, users):
         return self.request("Users.create", {
             "users": users
             })
