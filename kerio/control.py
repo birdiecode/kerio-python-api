15d74e36a051f9bfd2aeba333c4d1308309f1aa2
birdiecode <birdiecode@protonmail.com>
2022-02-04 17:06:51 +0300

    [ KerioControl ] добавлен метод getDhcp

--- kerio/control.py
+++ kerio/control.py
@@ -4,3 +4,11 @@ from .base import Kerio, PRODUCT_CONTROL
 class KerioControl(Kerio):
     def __init__(self, host, login, password, port=4081, authorization=True):
         super(KerioControl, self).__init__(PRODUCT_CONTROL, host, port, '/internal/dologin.php', login, password, authorization)
+
+    def getDhcp(self, start=0, limit=-1):
+        return self.request("Dhcp.get", {
+            "query": {
+                "start": start,
+                "limit": limit,
+            }
+        })['result']
